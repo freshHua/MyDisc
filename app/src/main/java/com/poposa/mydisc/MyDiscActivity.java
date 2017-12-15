18834bd5dc41ed80a7fa6370edab16b4d1468f01
Wu Jinghua <wjinghua@gmail.com>
2017-12-15 14:25:00 +0800

    Add empty activity.

---
+++ app/src/main/java/com/poposa/mydisc/MyDiscActivity.java
@@ -0,0 +1,13 @@
+package com.poposa.mydisc;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MyDiscActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
